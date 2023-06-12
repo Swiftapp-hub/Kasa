3f832a6f746c2c4d00c77e24d0c380ca188f52b6
SwiftApp <swiftyassistant.io@gmail.com>
2023-06-12 13:50:19 +0200

    Add base css class and create footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 import './css/Header.scss';
+import logo from '../assets/logo.webp';
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
-        <header>
+        <header className="flex align-center space-between">
+            <img src={logo} alt="Logo de Kasa"/>
+
+            <nav>
+                <NavLink className="nav-link font-normal" to="/" activeClassName="active">Accueil</NavLink>
+                <NavLink className="nav-link font-normal" to="/about" activeClassName="active">A Propos</NavLink>
+            </nav>
         </header>
     );
 }
